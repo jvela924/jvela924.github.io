7726b041349712c90c89ede84d4125092810486c
Jeremy Vela <jvela924@gmail.com>
2019-07-18 15:13:12 -0400

    added most html elements

--- settle-the-score-app/app.js
+++ settle-the-score-app/app.js
@@ -1,6 +1,6 @@
 
-$('form').on('submit', (event)=> {
-  event.preventDefault()
+$('#form1').on('submit', (event)=> {
+  event.preventDefault();
   let $userInput = $('input[type="text"]').val();
   console.log($userInput);
   $.ajax({
@@ -8,11 +8,36 @@ $('form').on('submit', (event)=> {
   }).then(
     (data)=>{
         $('#firstName1').html(data.data[0].team.full_name)
+        let playerID = (data.data[0].id)
         console.log(data.data[0].first_name);
-        let $playerID = (data.data[0].id);
-        console.log($playerID);
         $.ajax({
-          url:"https://www.balldontlie.io/api/v1/season_averages?player_ids[]=" + $playerID
+          url:"https://www.balldontlie.io/api/v1/season_averages?player_ids[]=" + playerID
+        }).then(
+          (data)=>{
+              console.log(data.data[0].pts);
+          }),
+          ()=>{
+              console.log('bad');
+          }
+    }),
+    ()=>{
+        console.log('bad');
+  }
+})
+
+$('#form1').on('submit', (event)=> {
+  event.preventDefault();
+  let $userInput = $('input[type="text"]').val();
+  console.log($userInput);
+  $.ajax({
+    url:"https://www.balldontlie.io/api/v1/players?search=" + $userInput
+  }).then(
+    (data)=>{
+        $('#firstName1').html(data.data[0].team.full_name)
+        let playerID = (data.data[0].id)
+        console.log(data.data[0].first_name);
+        $.ajax({
+          url:"https://www.balldontlie.io/api/v1/season_averages?player_ids[]=" + playerID
         }).then(
           (data)=>{
               console.log(data.data[0].pts);
