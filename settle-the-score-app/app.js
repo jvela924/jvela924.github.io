ee727e934e63978b75442f351b84a549349dc8d4
Jeremy Vela <jvela924@gmail.com>
2019-07-18 15:52:49 -0400

    can now enter player name and year to get average statistics and other data

--- settle-the-score-app/app.js
+++ settle-the-score-app/app.js
@@ -1,17 +1,19 @@
 
 $('#form1').on('submit', (event)=> {
   event.preventDefault();
-  let $userInput = $('input[type="text"]').val();
+  let $userInput = $('#player1').val();
   console.log($userInput);
+  let $season = $('#season1').val()
+  console.log($season);
   $.ajax({
-    url:"https://www.balldontlie.io/api/v1/players?search=" + $userInput
+    url:'https://www.balldontlie.io/api/v1/players?search=' + $userInput
   }).then(
     (data)=>{
         $('#firstName1').html(data.data[0].team.full_name)
         let playerID = (data.data[0].id)
         console.log(data.data[0].first_name);
         $.ajax({
-          url:"https://www.balldontlie.io/api/v1/season_averages?player_ids[]=" + playerID
+          url:'https://www.balldontlie.io/api/v1/season_averages?season=' + $season + '&player_ids[]=' + playerID
         }).then(
           (data)=>{
               console.log(data.data[0].pts);
