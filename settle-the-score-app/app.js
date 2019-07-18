72cc646e78ff7b2e127ae026fec920a963c1d0c1
Jeremy Vela <jvela924@gmail.com>
2019-07-18 12:40:59 -0400

    added additional ajax function to get season average statistics based on user input and that player's id

--- settle-the-score-app/app.js
+++ settle-the-score-app/app.js
@@ -1,14 +1,27 @@
-$(()=>{
 
+$('form').on('submit', (event)=> {
+  event.preventDefault()
+  let $userInput = $('input[type="text"]').val();
+  console.log($userInput);
   $.ajax({
-      url:"https://www.balldontlie.io/api/v1/players?search=lebron_james",
-      type: "GET",
+    url:"https://www.balldontlie.io/api/v1/players?search=" + $userInput
   }).then(
-      (data)=>{
-          $('#firstName1').html(data.data[0].team.full_name)
-          console.log(data.data[0].first_name);
-      }),
-      ()=>{
-          console.log('bad');
-      }
+    (data)=>{
+        $('#firstName1').html(data.data[0].team.full_name)
+        console.log(data.data[0].first_name);
+        let $playerID = (data.data[0].id);
+        console.log($playerID);
+        $.ajax({
+          url:"https://www.balldontlie.io/api/v1/season_averages?player_ids[]=" + $playerID
+        }).then(
+          (data)=>{
+              console.log(data.data[0].pts);
+          }),
+          ()=>{
+              console.log('bad');
+          }
+    }),
+    ()=>{
+        console.log('bad');
+  }
 })
