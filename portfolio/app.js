2091c8e5e07a2a07fac7e32b18cd810e9f9b1381
Jeremy Vela <jvela924@gmail.com>
2019-09-18 17:54:41 -0400

    added projects page

--- portfolio/app.js
+++ portfolio/app.js
@@ -6,10 +6,6 @@ const showMain = () => {
   $('#main').css('display', 'flex')
 }
 
-$('#about').on('click', hideMain);
-$('#about-button').on('click', hideMain);
-$('#home').on('click', showMain)
-
 const hideAboutMe = () => {
   $('#about-me').css('display', 'none')
 }
@@ -18,6 +14,29 @@ const showAboutMe = () => {
   $('#about-me').css('display', 'block')
 }
 
+const showProjects = () => {
+  $('#projects-div').css('display', 'block')
+}
+
+const hideProjects = () => {
+  $('#projects-div').css('display', 'none')
+}
+
+
+$('#about').on('click', hideMain);
+$('#about').on('click', hideProjects)
+$('#about-button').on('click', hideMain)
+$('#about-button').on('click', hideProjects)
+$('#home').on('click', showMain)
+$('#home').on('click', hideProjects)
+
 $('#about').on('click', showAboutMe)
 $('#home').on('click', hideAboutMe)
-$('#about-button').on('click', showAboutMe);
+$('#about-button').on('click', showAboutMe)
+
+$('#projects').on('click', showProjects)
+$('#projects').on('click', hideMain)
+$('#projects').on('click', hideAboutMe)
+
+$('#projects-button').on('click', showProjects)
+$('#projects-button').on('click', hideMain)
