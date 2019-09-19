962ded8bd49b90f574beb02da631b43589ef9319
Jeremy Vela <jvela924@gmail.com>
2019-09-19 11:19:46 -0400

    adding changes to portfolio

--- portfolio/app.js
+++ portfolio/app.js
@@ -22,21 +22,38 @@ const hideProjects = () => {
   $('#projects-div').css('display', 'none')
 }
 
+const hideResume = () => {
+  $('#resume-div').css('display', 'none')
+}
+
+const showResume = () => {
+  $('#resume-div').css('display', 'block')
+}
 
 $('#about').on('click', hideMain);
 $('#about').on('click', hideProjects)
 $('#about-button').on('click', hideMain)
 $('#about-button').on('click', hideProjects)
 $('#home').on('click', showMain)
+$('#home').on('click', hideResume)
 $('#home').on('click', hideProjects)
 
 $('#about').on('click', showAboutMe)
+$('#about').on('click', hideResume)
 $('#home').on('click', hideAboutMe)
 $('#about-button').on('click', showAboutMe)
+$('#about-button').on('click', hideResume)
 
 $('#projects').on('click', showProjects)
+$('#projects').on('click', hideResume)
 $('#projects').on('click', hideMain)
 $('#projects').on('click', hideAboutMe)
 
 $('#projects-button').on('click', showProjects)
 $('#projects-button').on('click', hideMain)
+$('#projects-button').on('click', hideResume)
+
+$('#resume').on('click', showResume)
+$('#resume').on('click', hideProjects)
+$('#resume').on('click', hideMain)
+$('#resume').on('click', hideAboutMe)
