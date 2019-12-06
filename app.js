9bc58fb46dac8d58ca193fe4ea5af8f62bf167b5
Jeremy Vela <jvela924@gmail.com>
2019-12-06 11:14:39 -0500

    added projects section

--- app.js
+++ app.js
@@ -5,3 +5,43 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+
+$("#project-div1").mouseenter(function(){
+  $('.project-title1').css("opacity", 1);
+})
+
+$("#project-div2").mouseenter(function(){
+  $('.project-title2').css("opacity", 1);
+})
+
+$("#project-div3").mouseenter(function(){
+  $('.project-title3').css("opacity", 1);
+})
+
+$("#project-div4").mouseenter(function(){
+  $('.project-title4').css("opacity", 1);
+})
+
+$("#project-div5").mouseenter(function(){
+  $('.project-title5').css("opacity", 1);
+})
+
+$("#project-div1").mouseleave(function(){
+  $('.project-title1').css("opacity", 0);
+})
+
+$("#project-div2").mouseleave(function(){
+  $('.project-title2').css("opacity", 0);
+})
+
+$("#project-div3").mouseleave(function(){
+  $('.project-title3').css("opacity", 0);
+})
+
+$("#project-div4").mouseleave(function(){
+  $('.project-title4').css("opacity", 0);
+})
+
+$("#project-div5").mouseleave(function(){
+  $('.project-title5').css("opacity", 0);
+})
